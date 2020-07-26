ff123433ec1920a291243022f13d1d839ac6c343
PapiHack <mssmbaye@gmail.com>
2020-07-26 01:53:01 +0000

    add str method in post model

--- synapp/blog/models.py
+++ synapp/blog/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.title
